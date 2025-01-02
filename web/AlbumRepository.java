b7efebb49289a1fa2f63b958dead480b6ce6fd88
Denis Lazarov <162356980+Duj0@users.noreply.github.com>
2025-01-02 23:49:24 +0200

    edit  for swagger documentation

---
+++ web/AlbumRepository.java
@@ -0,0 +1,9 @@
+//package bg.musicapp.musicdb.repository;
+import bg.musicapp.musicdb.model.entities.AlbumEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<AlbumEntity, Long> {
+
+}
