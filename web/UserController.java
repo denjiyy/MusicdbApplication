08c763331fc9aec3bf1935b212017bd0e02eed9b
Denis Tabutov <125385668+denjiyy@users.noreply.github.com>
2025-01-05 01:27:21 +0200

    Update UserController.java

--- web/UserController.java
+++ web/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-// these were all supposed to be just controllerc
+// these were all supposed to be just controllers
 @RestController
 @RequestMapping("/users")
 public class UserController {
